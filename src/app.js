ee4b3232ed23230c2f216e94d68890a357aecead
ravgw <raf.gwizdak@gmail.com>
2023-10-30 22:23:36 +0100

    home randm - capital info div

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
 import { embedHomeRandomCountry }  from './components/home-random.js'
-// import { getWeather }  from './api/api-utilities/weather.js'
-// import { } from './components/navigation.js'
 
 embedHomeRandomCountry()
 
