28908f31bd6a6710a47c0da9394d43cd971c4011
ravgw <raf.gwizdak@gmail.com>
2023-11-28 22:44:59 +0100

    app.js & home-action-buttons logic edit

--- src/app.js
+++ src/app.js
@@ -1,38 +1,6 @@
 import { embedHomeRandomCountry }  from './components/home-random.js'
-import { homeElements } from './home/home-elements.js'
+import { currentDisplay, addButtonsListeners} from './components/home-action-buttons.js'
 
 embedHomeRandomCountry()
 addButtonsListeners()
 
-function addButtonsListeners () {
-
-  document.querySelector('#btn-more_info').addEventListener('click', () => {
-    currentDisplay.actionBar.hide()
-  })
-  
-  document.querySelector('#btn-reload_country').addEventListener('click', () => {
-    embedHomeRandomCountry()
-  })
-  
-  document.querySelector('.back-button_section').addEventListener('click', () => {
-    currentDisplay.actionBar.show()
-  })
-}
-
-const currentDisplay = {
-  elements: homeElements,
-  actionBar: {
-    show() {
-      currentDisplay.elements.backButton.hide()
-      currentDisplay.elements.nameTagSection.show()
-      currentDisplay.elements.actionSection.show()
-
-    },
-    hide() {
-      currentDisplay.elements.nameTagSection.hide()
-      currentDisplay.elements.actionSection.hide()
-      currentDisplay.elements.backButton.show()
-
-    }
-  }
-}
--- src/components/home-action-buttons.js
+++ src/components/home-action-buttons.js
@@ -25,3 +25,47 @@ export function createDetailInfoBoard () {
 function showDetailInfoBoard () {
     homeElements.infoBoard?.show()
 }
+
+export function addButtonsListeners () {
+
+    document.querySelector('#btn-more_info').addEventListener('click', () => {
+      currentDisplay.actionBar.hide()
+      currentDisplay.elements.mainInfo.hide()
+    })
+    
+    document.querySelector('#btn-reload_country').addEventListener('click', () => {
+      embedHomeRandomCountry()
+    })
+    
+    document.querySelector('.back-button_section').addEventListener('click', () => {
+      currentDisplay.actionBar.show()
+    })
+  }
+  
+  document.querySelector('.home-random_main-info').addEventListener('animationend', (e) => {
+    if(e.target.classList.contains('animationHideByScale')){
+      currentDisplay.showDetailInfoBoard()
+    }
+  })
+   
+
+export const currentDisplay = {
+    elements: homeElements,
+    actionBar: {
+      show() {
+        currentDisplay.elements.backButton.hide()
+        currentDisplay.elements.nameTagSection.show()
+        currentDisplay.elements.actionSection.show()
+  
+      },
+      hide() {
+        currentDisplay.elements.nameTagSection.hide()
+        currentDisplay.elements.actionSection.hide()
+        currentDisplay.elements.backButton.show()
+      }
+    },
+    showDetailInfoBoard() {
+      createDetailInfoBoard()
+    }
+  }
+  
